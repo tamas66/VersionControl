91363b1d64e8ac359db93ad0589b3aa260d1acee
Tamas Tacsi <tamas512@gmail.com>
2022-09-30 14:29:51 +0200

    commit 6

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -95,7 +95,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(718, 293);
+            this.ClientSize = new System.Drawing.Size(748, 402);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
