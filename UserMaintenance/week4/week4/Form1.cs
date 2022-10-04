d5a5e56fab98118802db31aea19eaa39cc11589e
Tamas Tacsi <tamas512@gmail.com>
2022-10-05 00:21:31 +0200

    6

--- UserMaintenance/week4/week4/Form1.cs
+++ UserMaintenance/week4/week4/Form1.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace week4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            
+        }
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
