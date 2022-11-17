9686cd8b69eff657a1713edd1de1cbf091cc1ab0
Tamas Tacsi <tamas512@gmail.com>
2022-11-17 22:26:49 +0100

    commit2

--- week6.2/week6.2/Form1.cs
+++ week6.2/week6.2/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6._2.Entities;
 
 namespace week6._2
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
     }
 }
