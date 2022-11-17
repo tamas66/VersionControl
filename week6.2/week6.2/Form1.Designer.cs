45eaa437bded9b3167f6bdf66584692b5ddc8476
Tamas Tacsi <tamas512@gmail.com>
2022-11-17 23:13:32 +0100

    commit3

--- week6.2/week6.2/Form1.Designer.cs
+++ week6.2/week6.2/Form1.Designer.cs
@@ -29,12 +29,47 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(536, 279);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- week6.2/week6.2/Form1.cs
+++ week6.2/week6.2/Form1.cs
@@ -7,16 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6._2.Abstractions;
 using week6._2.Entities;
 
 namespace week6._2
 {
     public partial class Form1 : Form
     {
-        List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
+        List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
 
-        public BallFactory Factory
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -26,6 +27,32 @@ namespace week6._2
         {
             InitializeComponent();
             Factory = new BallFactory();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var toy = Factory.CreatNew();
+            _toys.Add(toy);
+            panel1.Controls.Add(toy);
+            toy.Left = -toy.Width;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var posi = 0; 
+            foreach (var toy in _toys)
+            {
+                toy.MoveToy();
+                if (toy.Left > posi)
+                    posi = toy.Left;
+            }
+
+            if(posi >= 1000)
+            {
+                var first = _toys[0];
+                panel1.Controls.Remove(first);
+                _toys.Remove(first);
+            }
         }
     }
 }
