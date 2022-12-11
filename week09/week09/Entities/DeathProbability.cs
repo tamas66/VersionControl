2bc38307833f065e4e252204ac9828d4ee8a73c8
Tamas Tacsi <tamas512@gmail.com>
2022-12-11 11:27:28 +0100

    commit 2

---
+++ week09/week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week09.Entities
+{
+    public class DeathProbability
+    {
+        public int Kor { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
---
+++ week09/week09/Entities/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week09.Entities
+{
+    public enum Gender
+    {
+        Male =1, Female =2
+    }
+}
